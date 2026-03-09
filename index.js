f580d324d61ea7e47ea9d844a063beeabea0eccb
Naza541 <princeimpact18@gmail.com>
2026-03-09 14:03:57 +0100

    chore

---
+++ index.js
@@ -0,0 +1,8 @@
+// This is my first JavaScript code, I am learning how to use it to make my website more interactive and dynamic.
+console.log("Welcome to DigiTech");
+
+let name = "DigiTech Hub";
+console.log(name);
+
+let founder = "Princewill";
+console.log();
